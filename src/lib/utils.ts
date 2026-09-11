8ace011be37f047357379315dbd503a4d7e78a81
sjfoodsonline <sjfoodonline@gmail.com>
2026-09-11 06:07:50 +0500

    Add files via upload

---
+++ src/lib/utils.ts
@@ -0,0 +1,15 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatPkr(amount: number) {
+  return `Rs. ${amount.toLocaleString("en-PK")}`;
+}
+
+export function waOrderUrl(phone: string, message: string) {
+  const digits = phone.replace(/\D/g, "");
+  return `https://wa.me/${digits}?text=${encodeURIComponent(message)}`;
+}
